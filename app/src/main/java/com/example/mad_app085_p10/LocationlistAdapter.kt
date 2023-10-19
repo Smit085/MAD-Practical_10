dd529684274c6875b5df77a0f30345e7e0a4af6d
student <student@gnu.ac.in>
2023-10-19 12:35:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_app085_p10/LocationlistAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.mad_app085_p10
+
+import androidx.recyclerview.widget.RecyclerView
+
+class LocationlistAdapter: RecyclerView<LocationlistCardModel> {
+}
---
+++ app/src/main/java/com/example/mad_app085_p10/LocationlistCardModel.kt
@@ -0,0 +1,13 @@
+package com.example.mad_app085_p10
+
+class LocationlistCardModel {
+    var name = ""
+    var mobno = ""
+    var address = ""
+
+    constructor(name: String, mobno: String, address: String){
+        this.name = name
+        this.mobno = mobno
+        this.address = address
+    }
+}
